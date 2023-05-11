5458b2c418b291a551096ec4a9a0223ea3958a09
Erkan Bostan <profesyonel59@gmail.com>
2023-05-11 21:15:37 +0300

    Comment Transactions Completed

--- TraversalSite/Areas/Admin/Controllers/CommentController.cs
+++ TraversalSite/Areas/Admin/Controllers/CommentController.cs
@@ -9,16 +9,29 @@ namespace TraversalSite.Areas.Admin.Controllers
     public class CommentController : Controller
     {
         private readonly ICommentService _commentService;
+        private readonly IUserService _userService;
 
-        public CommentController(ICommentService commentService)
+        public CommentController(ICommentService commentService, IUserService userService)
         {
+            _userService = userService;
             _commentService = commentService;
         }
 
         public IActionResult Index()
         {
             var values = _commentService.TGetCommentByDetail();
-            return View(values) ;
+            return View(values);
+        }
+        public IActionResult DeleteComment(int id)
+        {
+            _commentService.YouCanDelete(_commentService.YouCanGetById(id));
+            return RedirectToAction("Index");
+        }
+        public IActionResult CommentDetail(int id)
+        {
+            var comment = _commentService.YouCanGetById(id);
+            ViewBag.user = _userService.YouCanGetById(comment.AppUserId).Name;
+            return View(comment);
         }
     }
 }
